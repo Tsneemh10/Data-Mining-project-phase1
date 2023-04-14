87cd09ca94242738d32e1c8bab0ae8e4607b82da
Tsneemh10 <68311792+Tsneemh10@users.noreply.github.com>
2023-04-14 18:16:24 +0200

    commented the encoding lines

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 df= pd.read_csv('Mall_Customers.csv')
-df['Gender']= df['Gender'].astype('category')
-df['Gender']= df['Gender'].cat.codes
+# df['Gender']= df['Gender'].astype('category')
+# df['Gender']= df['Gender'].cat.codes
 corrMatrix= df.corr()
 ax= plt.figure().add_subplot()
 sns.heatmap(corrMatrix, cmap='RdBu', vmin=-0.5, vmax=0.5, annot=True)
